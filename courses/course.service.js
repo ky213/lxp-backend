2c54384fb7be8f341a0fbb464ceecb0834b3b434
Kovacevic Damir <damir.eb@gmail.com>
2020-04-16 00:16:17 +0200

    Course changes

--- courses/course.service.js
+++ courses/course.service.js
@@ -12,13 +12,44 @@ module.exports = {
 };
 
 async function getAll(loggedInUser, selectedInstituteId) {
-  return knex('courses');
-}
-
-async function getById(loggedInUser, selectedInstituteId, courseId) {
+  if (!loggedInUser)
+    return;
+  
+  let instituteId = (loggedInUser.role == Role.SuperAdmin && selectedInstituteId) ? selectedInstituteId : loggedInUser.institute;
 
   return knex('courses')
-    .where('course_id', courseId);
+    .join('programs', 'programs.program_id', 'courses.program_id')
+    .select([
+        'courses.course_id as courseId',
+        'courses.institute_id as instituteId',
+        'courses.name as name',
+        'courses.period_days as periodDays',
+        'courses.starting_date as startingDate',
+        'programs.name as programName',
+    ]);
+}
+
+async function getById(loggedInUser, courseId, selectedInstituteId) {
+  if (!loggedInUser)
+    return;
+  
+  let instituteId = (loggedInUser.role == Role.SuperAdmin && selectedInstituteId) ? selectedInstituteId : loggedInUser.institute;
+
+  return knex('courses')
+    .join('programs', 'programs.program_id', 'courses.program_id')
+    .where('courses.course_id', courseId)
+    .select([
+        'courses.course_id as courseId',
+        'courses.institute_id as instituteId',
+        'courses.name as name',
+        'courses.description as description',
+        'courses.period_days as periodDays',
+        'courses.starting_date as startingDate',
+        'courses.program_id as programId',
+        'programs.name as programName',
+    ])
+    .limit(1)
+    .first();
 }
 
 async function getByUser(loggedInUser, includeRead, selectedInstituteId) {
@@ -32,30 +63,46 @@ async function getByUser(loggedInUser, includeRead, selectedInstituteId) {
     .where('course_id', courseId);
 }
 
-async function create(loggedInUser, data) {
-  console.log('loggedInUser', loggedInUser);
+async function create(loggedInUser, data, selectedInstituteId) {
+  if (!loggedInUser)
+    return;
+
+  let instituteId = (loggedInUser.role == Role.SuperAdmin && selectedInstituteId) ? selectedInstituteId : loggedInUser.institute;
+
   return knex("courses")
     .insert({
       institute_id: data.instituteId,
       program_id: data.programId,
       name: data.name,
       description: data.description,
-      // content_path
-      // image: 
+      content_path: data.contentPath,
+      image: 'test',
       period_days: data.periodDays,
       starting_date: data.startingDate,
       generated: new Date()
     });
 }
 
-async function update(loggedInUser, data) {
+async function update(loggedInUser, data, selectedInstituteId) {
+  if (!loggedInUser)
+    return;
+
+  let instituteId = (loggedInUser.role == Role.SuperAdmin && selectedInstituteId) ? selectedInstituteId : loggedInUser.institute;
+
   console.log('update', data);
-  // return knex
-  // .transaction(async function(t) {
-  //   await knex("courses")
-  //       .transacting(t)
-  //       .where('course_id', data.courseId);        
-  // });
+
+  return knex("courses")
+    .where('course_id', data.courseId)
+    .update({
+      program_id: data.programId,
+      name: data.name,
+      description: data.description,
+      content_path: data.contentPath,
+      image: 'test',
+      period_days: data.periodDays,
+      starting_date: data.startingDate,
+      generated: new Date()      
+    });
 }
 
 async function deleteCourse(loggedInUser, id) {
