8cee34d3328a5ad828f3f4c4e4cd57de962ad14d
MAIS-RADWAN <mais.radwan@gmail.com>
2020-08-31 10:31:05 +0300

    LXQ-41 : Program Manager (Group): When navigate to the 'Group' link, the page refreshes.

--- groups/groups.controller.js
+++ groups/groups.controller.js
@@ -5,11 +5,11 @@ const authorize = require('helpers/authorize')
 const Role = require('helpers/role');
 
 // routes
-router.post('/', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager]), create); 
-router.put('/', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager]), update); 
+router.post('/', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager, Role.ProgramDirector]), create); 
+router.put('/', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager, Role.ProgramDirector]), update); 
 router.delete('/', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager]), deletegroups); 
-router.get('/', authorize([Role.SuperAdmin]), getAll); 
-router.get('/:id', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager]), getById);  
+router.get('/', authorize(), getAll); 
+router.get('/:id', authorize([Role.Admin, Role.SuperAdmin, Role.LearningManager, Role.ProgramDirector]), getById);  
 module.exports = router;
 
 function getAll(req, res, next) {
