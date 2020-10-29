d84afedfbbfc64ae597ea10fea66944bc032fee7
Tomasz Wojciechowski <tomasz.wojciechowski@alwasaet.com>
2020-10-29 12:10:02 +0100

    update cors settings

--- cloud-storage-conf/updateCorsOnStorage.sh
+++ cloud-storage-conf/updateCorsOnStorage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gsutil cors set development-cors.json gs://lxp-uploads-development/
+#gsutil cors set development-cors.json gs://lxp-uploads-development/
 gsutil cors set staging-cors.json gs://lxp-uploads-staging/
